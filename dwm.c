19973276a2ad8da21dfca0913882664fda0562db
agus <33077305+agusc01@users.noreply.github.com>
2025-03-24 17:38:44 -0300

    patch dwm-nobroderfloating

--- dwm.c
+++ dwm.c
@@ -1082,6 +1082,14 @@ manage(Window w, XWindowAttributes *wa)
 	c->bw = c->mon->borderpx;
 
 	wc.border_width = c->bw;
+	if (((nexttiled(c->mon->clients) == c && !nexttiled(c->next))
+	    || &monocle == c->mon->lt[c->mon->sellt]->arrange)
+	    && !c->isfullscreen && !c->isfloating
+	    && NULL != c->mon->lt[c->mon->sellt]->arrange) {
+		c->w = wc.width += c->bw * 2;
+		c->h = wc.height += c->bw * 2;
+		wc.border_width = 0;
+	}
 	XConfigureWindow(dpy, w, CWBorderWidth, &wc);
 	XSetWindowBorder(dpy, w, scheme[SchemeNorm][ColBorder].pixel);
 	configure(c); /* propagates border_width, if size doesn't change */
