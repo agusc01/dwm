f8fdca3a7996b17e7008529dbe1e6ad08733c687
agus <33077305+agusc01@users.noreply.github.com>
2025-03-28 18:57:07 -0300

    update to use dmenu-desktoponly

--- config.def.h
+++ config.def.h
@@ -94,7 +94,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { 
-    "dmenu_run",
+    "dmenu_run_desktop",
     "-p", "Run:",
     "-m", dmenumon,
     "-fn", dmenufont,
