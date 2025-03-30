77291b197b665f9c2bd2e634047b848b94eb6dba
agus <33077305+agusc01@users.noreply.github.com>
2025-03-30 13:33:40 -0300

    without dmenu

--- config.h
+++ config.h
@@ -92,22 +92,8 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/usr/bin/fish", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { 
-    "dmenu_run_desktop",
-    "-p", "Run:",
-    "-m", dmenumon,
-    "-fn", dmenufont,
-    "-nb", col_gray1,
-    "-nf", col_gray3,
-    "-sb", col_cyan,
-    "-sf", col_gray4,
-    NULL
-};
-
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
     { MODKEY,                       XK_u,      spawn,          SHCMD("dshortcuts") },
 	{ MODKEY,                       XK_y,      spawn,          SHCMD("st") },
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          SHCMD("slock") },
