87c5703a2d31080b0414c57032db8c492739f5f3
agus <33077305+agusc01@users.noreply.github.com>
2025-04-06 10:39:48 -0300

    Inicial borderpx & smartgaps

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #define FONT "Ubuntu:size=12"
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int scalepreview       = 4;        /* preview scaling (display w and h / scalepreview) */
 static const int previewbar         = 1;        /* show the bar in the preview window */
@@ -11,7 +11,7 @@ static const unsigned int gappiv    = 10;       /* vert inner gap between window
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static       int smartborders       = 1;        /* 1 means no outer gap when there is only one window */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 18;       /* 2 is the default spacing around the bar's font */
