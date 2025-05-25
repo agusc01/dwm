d99c0b517c9b77f28b6063b516719e6c8f258875
agus <33077305+agusc01@users.noreply.github.com>
2025-05-25 00:15:11 -0300

    Better launchers

--- launchers.h
+++ launchers.h
@@ -1,15 +1,16 @@
 /* launcher commands (They must be NULL terminated) */
 static const char* terminal[]     = { "st", NULL };
 static const char* onboard[]      = { "fish", "-c", "oonboard", NULL };
-static const char* apps[]         = { "rofi", "-show", "drun", NULL };
+static const char* apps[]         = { "fish", "-c", "rofi_launcher", NULL };
 static const char* firefox[]      = { "firefox", "--private-window", NULL };
 static const char* wifi[]         = { "fish", "-c", "wifi rofi both", NULL };
 static const char* bluetooth[]    = { "fish", "-c", "bluetooth rofi both", NULL };
-static const char* brightness[]   = { "fish", "-c", "brightness rofi both", NULL };
-static const char* sound[]        = { "fish", "-c", "sound rofi both", NULL };
-static const char* rotation[]     = { "fish", "-c", "rotation rofi both", NULL };
+static const char* brightness[]   = { "fish", "-c", "rofi_brightness", NULL };
+static const char* volume[]       = { "fish", "-c", "rofi_volume", NULL };
+static const char* rotation[]     = { "fish", "-c", "rofi_rotation", NULL };
 static const char* thunar[]       = { "thunar", NULL };
 static const char* galculator[]   = { "galculator", NULL };
+static const char* power[]        = { "fish", "-c", "rofi_power", NULL };
 
 static const Launcher launchers[] = {
      /* command       name to display */
@@ -20,8 +21,9 @@ static const Launcher launchers[] = {
 	{ wifi,                 "" },
 	{ bluetooth,            "" },
 	{ brightness,           "" },
-	{ sound,                "" },
-	{ rotation,             "" },
+	{ volume,               "" },
+	{ rotation,             "" },
 	{ thunar,               "" },
 	{ galculator,           "" },
+	{ power,                "" },
 };
