8a5a3a649c3620a20a64ef941e94ce8f37679933
agus <33077305+agusc01@users.noreply.github.com>
2025-05-26 21:08:17 -0300

    Add more launchers

--- launchers.h
+++ launchers.h
@@ -2,12 +2,15 @@
 static const char* terminal[]     = { "st", NULL };
 static const char* onboard[]      = { "fish", "-c", "oonboard", NULL };
 static const char* apps[]         = { "fish", "-c", "rofi_launcher", NULL };
-static const char* firefox[]      = { "firefox", "--private-window", NULL };
+static const char* internet[]     = { "fish", "-c", "rofi_internet", NULL };
 static const char* wifi[]         = { "fish", "-c", "wifi rofi both", NULL };
 static const char* bluetooth[]    = { "fish", "-c", "bluetooth rofi both", NULL };
 static const char* brightness[]   = { "fish", "-c", "rofi_brightness", NULL };
 static const char* volume[]       = { "fish", "-c", "rofi_volume", NULL };
+static const char* microphone[]   = { "fish", "-c", "rofi_microphone", NULL };
+static const char* camera[]       = { "fish", "-c", "rofi_camera", NULL };
 static const char* rotation[]     = { "fish", "-c", "rofi_rotation", NULL };
+static const char* layout[]       = { "fish", "-c", "rofi_layout", NULL };
 static const char* thunar[]       = { "thunar", NULL };
 static const char* galculator[]   = { "galculator", NULL };
 static const char* power[]        = { "fish", "-c", "rofi_power", NULL };
@@ -17,13 +20,16 @@ static const Launcher launchers[] = {
     { terminal,             "" },
 	{ onboard,              "" },
 	{ apps,                 "" },
-    { firefox,              "" },
+    { internet,             "" },
 	{ wifi,                 "" },
 	{ bluetooth,            "" },
 	{ brightness,           "" },
 	{ volume,               "" },
+	{ microphone,           "" },
+	{ camera,               "" },
 	{ rotation,             "" },
-	{ thunar,               "" },
-	{ galculator,           "" },
+	{ layout,               "" },
+    { thunar,               "" },
+    { galculator,           "" },
 	{ power,                "" },
 };
