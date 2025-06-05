8d55fcabe4e605eb46f711b259b982f4b8919a4c
agus <33077305+agusc01@users.noreply.github.com>
2025-06-05 11:58:10 +1000

    change rules

--- rules.h
+++ rules.h
@@ -7,8 +7,8 @@ static const Rule rules[] = {
 	// { "vim",                    NULL,       NULL,       0,            0,           -1 },
 	// { "nvim",                   NULL,       NULL,       0,            0,           -1 },
     // { "st",                     NULL,       NULL,       1 << 1,          0,           -1 },
-	{ "firefox-esr",            NULL,       NULL,       1 << 2,          0,           -1 },
-	{ "Surf",                   NULL,       NULL,       1 << 2,          0,           -1 },
+	// { "firefox-esr",            NULL,       NULL,       1 << 2,          0,           -1 },
+	// { "Surf",                   NULL,       NULL,       1 << 2,          0,           -1 },
     // { "Thunar",                 NULL,       NULL,       1 << 3,          0,           -1 },
     { "Gimp",                   NULL,       NULL,       1 << 7,          0,           -1 },
 };
