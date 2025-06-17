8d3ad9b3aefa34f8c1420c58758b7a352e0f922a
agus <33077305+agusc01@users.noreply.github.com>
2025-06-17 18:22:27 -0300

    Take off obsolete code

--- rules.h
+++ rules.h
@@ -4,11 +4,5 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class                    instance    title       tags mask     isfloating   monitor */
-	// { "vim",                    NULL,       NULL,       0,            0,           -1 },
-	// { "nvim",                   NULL,       NULL,       0,            0,           -1 },
-    // { "st",                     NULL,       NULL,       1 << 1,          0,           -1 },
-	// { "firefox-esr",            NULL,       NULL,       1 << 2,          0,           -1 },
-	// { "Surf",                   NULL,       NULL,       1 << 2,          0,           -1 },
-    // { "Thunar",                 NULL,       NULL,       1 << 3,          0,           -1 },
     { "Gimp",                   NULL,       NULL,       1 << 7,          0,           -1 },
 };
