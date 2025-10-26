5f5235dc115de079fbdff001701455ce1754f29b
agus <33077305+agusc01@users.noreply.github.com>
2025-10-26 20:50:13 -0300

    calculator key (shortcut)

--- keys.h
+++ keys.h
@@ -31,6 +31,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_y,      spawn,          SHCMD("rofi_menu") },
 	{ MODKEY,                       XK_u,      spawn,          SHCMD("rofi_launcher") },
     { MODKEY,                       XK_Print,  spawn,          SHCMD("flameshot gui") },
+    { MODKEY,                       XK_F12,    spawn,          SHCMD("galculator") },
 	{ MODKEY|ControlMask|ShiftMask|AltMask, XK_l, spawn,       SHCMD("slock") },
 
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
